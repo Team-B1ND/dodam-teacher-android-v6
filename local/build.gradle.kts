672c2b6578b1f1189c4f8b53f5b48f4a2563e7c7
최민재 <cmjcmj1230@gmail.com>
2023-01-19 16:04:24 +0900

    ⚡️ :: Add remote and local module

---
+++ local/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id(Plugins.androidLibrary)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kotlinKapt)
+    id(Plugins.daggerPlugin)
+    id(Plugins.kotlinParcelize)
+    id(Plugins.kt_lint) version Versions.KT_LINT
+}
+
+android {
+    namespace = ProjectProperties.NAME_SPACE_DATA
+    compileSdk = ProjectProperties.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        minSdk = ProjectProperties.MIN_SDK_VERSION
+        targetSdk = ProjectProperties.TARGET_SDK_VERSION
+
+        testInstrumentationRunner = ProjectProperties.TEST_RUNNER
+    }
+
+    compileOptions {
+        sourceCompatibility = ProjectProperties.JAVA_VERSION
+        targetCompatibility = ProjectProperties.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = ProjectProperties.JVM_TARGET
+    }
+    packagingOptions {
+        resources {
+            excludes += ProjectProperties.EXCLUDES
+        }
+    }
+}
+
+dependencies {
+    implementation(AndroidX.CORE_KTX)
+    implementation(AndroidX.LIFECYCLE_KTX)
+    testImplementation(UnitTest.JUNIT)
+    androidTestImplementation(AndroidTest.ANDROID_JUNIT)
+    androidTestImplementation(AndroidTest.ESPRESSO_CORE)
+
+    // coroutine
+    implementation(Kotlin.COROUTINES_ANDROID)
+    implementation(Kotlin.COROUTINES_CORE)
+
+    // retrofit
+    implementation(Libraries.RETROFIT)
+    implementation(Libraries.RETROFIT_CONVERTER_GSON)
+    implementation(Libraries.OKHTTP)
+    implementation(Libraries.OKHTTP_LOGGING_INTERCEPTOR)
+
+    // hilt
+    implementation(Google.HILT_ANDROID)
+    kapt(Google.HILT_ANDROID_COMPILER)
+}
---
+++ local/src/main/java/kr/hs/dgsw/smartschool/local/MyClass.kt
@@ -0,0 +1,4 @@
+package kr.hs.dgsw.smartschool.local
+
+class MyClass {
+}
