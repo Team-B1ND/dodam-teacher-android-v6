003343acca68b1a8f82ca09687879278f537e85e
최민재 <cmjcmj1230@gmail.com>
2023-03-03 10:31:26 +0900

    ⚡️ :: add token interceptor auto fetch

--- remote/src/main/java/kr/hs/dgsw/smartschool/remote/interceptor/TokenInterceptor.kt
+++ remote/src/main/java/kr/hs/dgsw/smartschool/remote/interceptor/TokenInterceptor.kt
@@ -3,49 +3,79 @@ package kr.hs.dgsw.smartschool.remote.interceptor
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kr.hs.dgsw.smartschool.data.datasource.auth.AuthCacheDataSource
 import kr.hs.dgsw.smartschool.domain.exception.ExpiredRefreshTokenException
 import kr.hs.dgsw.smartschool.domain.model.token.Token
-import kr.hs.dgsw.smartschool.domain.repository.AuthRepository
-import kr.hs.dgsw.smartschool.domain.repository.TokenRepository
+import kr.hs.dgsw.smartschool.domain.usecase.auth.LoginUseCase
 import kr.hs.dgsw.smartschool.domain.usecase.token.FetchTokenUseCase
 import kr.hs.dgsw.smartschool.domain.usecase.token.GetTokenUseCase
-import kr.hs.dgsw.smartschool.remote.url.DodamUrl
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 class TokenInterceptor @Inject constructor(
     private val getTokenUseCase: GetTokenUseCase,
     private val fetchTokenUseCase: FetchTokenUseCase,
+    private val loginUseCase: LoginUseCase,
+    private val authCacheDataSource: AuthCacheDataSource,
 ) : Interceptor {
 
     private val TOKEN_HEADER = "Authorization"
-
-    private val postMethod = "POST"
-
-    private val emptyTokenRequest = listOf(
-        DodamUrl.Auth.LOGIN,
-        DodamUrl.Auth.JOIN,
-    )
+    private val TOKEN_ERROR = 401
 
     private lateinit var token: Token
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
-        var request = chain.request()
+        runBlocking(Dispatchers.IO) {
+            getTokenUseCase().onSuccess {
+                token = it
+            }.onFailure {
+                throw ExpiredRefreshTokenException()
+            }
+        }
 
-        val method = request.method
 
-        if (method != postMethod && emptyTokenRequest.contains(request.url.encodedPath).not()) {
+        val request: Request = chain.request().newBuilder()
+            .addHeader(TOKEN_HEADER, "Bearer ${token.token}")
+            .build()
+
+        var response = chain.proceed(request)
+        if (response.code == TOKEN_ERROR) {
             runBlocking(Dispatchers.IO) {
-                getTokenUseCase().onSuccess {
-                    token = it
+                fetchTokenUseCase().onSuccess {
+                    val refreshRequest: Request = chain.request().newBuilder()
+                        .addHeader(TOKEN_HEADER, "Bearer ${it.token}")
+                        .build()
+                    response = chain.proceed(refreshRequest)
+
+                    if (response.code == TOKEN_ERROR) {
+                        runBlocking(Dispatchers.IO) {
+                            val account = authCacheDataSource.getAccount()
+                            loginUseCase(
+                                LoginUseCase.Param(
+                                    id = account.id ?: "",
+                                    pw = account.pw ?: "",
+                                    enableAutoLogin = true,
+                                )
+                            ).onSuccess { loginToken ->
+                                val loginRequest: Request = chain.request().newBuilder()
+                                    .addHeader(TOKEN_HEADER, "Bearer ${loginToken.token}")
+                                    .build()
+                                response = chain.proceed(loginRequest)
+
+                                if (response.code == TOKEN_ERROR) {
+                                    throw ExpiredRefreshTokenException()
+                                }
+                            }.onFailure {
+                                throw ExpiredRefreshTokenException()
+                            }
+                        }
+                    }
                 }.onFailure {
                     throw ExpiredRefreshTokenException()
                 }
             }
-            request = chain.request().newBuilder()
-                .addHeader(TOKEN_HEADER, "Bearer ${token.token}")
-                .build()
         }
 
         return chain.proceed(request)
