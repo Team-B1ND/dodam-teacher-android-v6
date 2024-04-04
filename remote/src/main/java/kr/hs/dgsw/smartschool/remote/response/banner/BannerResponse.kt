a4cae468b089d71d575c06278393a6ba84945d55
8954sood <58807462+8954sood@users.noreply.github.com>
2024-04-04 20:54:51 +0900

    🐛 :: fix banner feature

--- remote/src/main/java/kr/hs/dgsw/smartschool/remote/response/banner/BannerResponse.kt
+++ remote/src/main/java/kr/hs/dgsw/smartschool/remote/response/banner/BannerResponse.kt
@@ -3,11 +3,9 @@ package kr.hs.dgsw.smartschool.remote.response.banner
 import com.google.gson.annotations.SerializedName
 
 data class BannerResponse(
-    @field:SerializedName("bannerOrder") val bannerOrder: Int,
-    @field:SerializedName("createdDate") val createdDate: String,
-    @field:SerializedName("expiryDateTime") val expiryDateTime: String,
+    @field:SerializedName("expireAt") val expiryDateTime: String,
     @field:SerializedName("id") val id: Int,
-    @field:SerializedName("image") val image: String,
+    @field:SerializedName("imageUrl") val image: String,
     @field:SerializedName("redirectUrl") val redirectUrl: String,
     @field:SerializedName("status") val status: BannerResponseStatus,
     @field:SerializedName("title") val title: String,
