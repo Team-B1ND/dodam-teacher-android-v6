06046664dc322bd53f6b9a9ddb3c5a3e968a020d
8954sood <58807462+8954sood@users.noreply.github.com>
2024-04-05 09:19:35 +0900

    🐛 :: fix meal multiple get

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam_teacher/features/meal/screen/MealScreen.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam_teacher/features/meal/screen/MealScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -75,8 +76,9 @@ fun MealScreen(
         iterations = LottieConstants.IterateForever
     )
 
-    mealViewModel.getMeal(state.currentDate)
-    mealViewModel.getCalorie(state.currentDate)
+    LaunchedEffect(key1 = state.currentDate) {
+        mealViewModel.getMeal(state.currentDate)
+    }
 
     val scrollState = rememberScrollState()
 
